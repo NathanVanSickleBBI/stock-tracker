9bc6b48f0290574ab0c3c25622a750e1f4147b71
Nathan VanSickle <nvansickle@b-paid.com>
2025-07-17 14:17:16 -0400

    add button to randomly change price

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,23 @@
-const priceChangeButton = document.querySelector('.priceChangeButton')
+const priceChangeButton = document.querySelector(".priceChangeButton");
 
-priceChangeButton.addEventListener('click', (e) => {
-    let stockCards = document.querySelectorAll('.stock')
-    // console.log(stockCards)
-    stockCards.forEach(card => {
-        let price = Number(card.children[2].innerHTML)
-        let percentChange = Math.random() * 10
-        // card.children[3].innerHTML = percentChange.toFixed(2)
-        let isPositive = Math.random() > 0.5
-        let change = price * (percentChange / 100)
-        card.children[2].innerHTML = (isPositive ? price + change : price - change).toFixed(2)
-    })
-})
+priceChangeButton.addEventListener("click", (e) => {
+  const stockCards = document.querySelectorAll(".stock");
+
+  stockCards.forEach((card) => {
+    const priceElement = card.querySelector(".price");
+    const changeElement = card.querySelector(".change");
+
+    const price = parseFloat(Number(priceElement.textContent).toFixed(2));
+    const percentChange = Math.random() * 10;
+    const isPositive = Math.random() > 0.5;
+    const delta = price * (percentChange / 100);
+
+    priceElement.textContent = `${
+      isPositive ? (price + delta).toFixed(2) : (price - delta).toFixed(2)
+    }`;
+
+    changeElement.textContent = `${isPositive ? "+" : "-"}${parseFloat(
+      percentChange
+    ).toFixed(2)}% today`;
+  });
+});
